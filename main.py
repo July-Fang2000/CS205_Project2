d5f633a8c5dbe61ec77a7bc29ea34cbd97615807
July-Fang2000 <zfang052@ucr.edu>
2023-05-24 17:51:06 -0700

    init: add dataset and simple text line interface

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to Zelai Fang and Hengshuo Zhang Feature Selection Algorithm.")
+filename = input("Type in the name of the file to test: ")
+method = input("Type the number of the algorithm you want to run.\n"
+               "     1) Forward Selection\n"
+               "     2) Backward Selection\n")
+
