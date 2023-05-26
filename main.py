d10e685cb1734ceb78003c767e1cda2724129b09
July-Fang2000 <zfang052@ucr.edu>
2023-05-26 15:00:37 -0700

    add: implement feature search

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from method import *
 print("Welcome to Zelai Fang and Hengshuo Zhang Feature Selection Algorithm.")
 filename = input("Type in the name of the file to test: ")
 if filename == "1":
-    filename = "CS170_small_Data__27.txt"
+    filename = "CS170_small_Data__32.txt"
 elif filename == "2":
-    filename = "CS170_large_Data__30.txt"
+    filename = "CS170_small_Data__33.txt"
 elif filename == "3":
-    filename = "CS170_XXXlarge_Data__17.txt"
+    filename = "CS170_large_Data__32.txt"
+elif filename == "4":
+    filename = "CS170_large_Data__33.txt"
 
 method = input("Type the number of the algorithm you want to run.\n"
                "     1) Forward Selection\n"
@@ -18,5 +20,5 @@ datas, labels = read_txt(filename)
 feature_size = len(datas[0])
 instance_size = len(datas)
 datas = normalized_data(datas)
-
-search(method, feature_size, instance_size, accuracy=NearestNeighborClassifier(datas, labels).accuracy())
+classifier = NearestNeighborClassifier(datas, labels)
+search(method, feature_size, instance_size, classifier)
--- method.py
+++ method.py
@@ -23,26 +23,93 @@ class NearestNeighborClassifier:
         for i in range(len(self.datas)):
             if self.labels[i] == self.predict(i):
                 correct += 1
-        return (correct / len(self.datas)) * 100
+        return round((correct / len(self.datas)) * 100, 1)
+
+    def accuracy_with_features(self, features):
+        datas_sub = self.datas[:, features]
+        sub_classifier = NearestNeighborClassifier(datas_sub, self.labels)
+        return sub_classifier.accuracy()
 
 
-def search(method, feature_size, instance_size, accuracy):
+def search(method, feature_size, instance_size, classifier):
     print("This dataset has {} features(not including the class attribute), with {} instances."
           .format(feature_size, instance_size))
     print("Running nearest neighbor with all {} features, "
-          "using \"leaving-one-out\" evaluation, I get an accuracy of {}%".format(feature_size, accuracy))
+          "using \"leaving-one-out\" evaluation, I get an accuracy of {}%".format(feature_size, classifier.accuracy()))
     print("Beginning search.")
     if method == "1":
-        forward_selection()
+        forward_selection(classifier, feature_size)
     elif method == "2":
-        backward_elimination()
+        backward_elimination(classifier, feature_size)
     else:
         print("Choose Wrong Method! Please type 1 or 2!")
 
 
-def forward_selection():
-    return 0
+def explore_features(set, size, method):
+    sets = []
+    if method == "forward":
+        for i in range(size):
+            cur_set = list(set)
+            if i not in cur_set:
+                cur_set.append(i)
+                sets.append(cur_set)
+    elif method == "backward":
+        for i in range(size):
+            cur_set = list(set)
+            if i in cur_set:
+                cur_set.remove(i)
+                sets.append(cur_set)
+    else:
+        raise ValueError("Invalid method. Choose 'forward' or 'backward'.")
+    return sets
 
 
-def backward_elimination():
-    return 0
+def forward_selection(classifier, size):
+    max_accuracy = -2
+    set = []
+    for i in range(size):
+        curr_accuracy = -1
+        sets = explore_features(set, size, "forward")
+        for expanded in sets:
+            accuracy = classifier.accuracy_with_features(expanded)
+            print("Using feature(s) {", ','.join(map(str, [feature + 1 for feature in expanded])), "} accuracy is",
+                  accuracy, "%")
+            if accuracy > curr_accuracy:
+                curr_accuracy = accuracy
+                set = expanded
+        print("")
+        if curr_accuracy > max_accuracy:
+            max_accuracy = curr_accuracy
+            max_set = set
+        else:
+            print("(Warning, Accuracy has decreased! Continuing search in case of local maxima)")
+        print("Feature set {", ','.join(map(str, [feature + 1 for feature in set])), "} was best, accuracy is",
+              curr_accuracy, "%\n")
+    print("Finished search!! The best feature subset is {", ','.join(map(str, [feature + 1 for feature in max_set])),
+          "}, which has an accuracy of", max_accuracy, "%\n")
+
+
+def backward_elimination(classifier, size):
+    max_accuracy = -2
+    set = list(range(size))
+    for i in range(size-1):
+        curr_accuracy = -1
+        sets = explore_features(set, size, "backward")
+        for expanded in sets:
+            accuracy = classifier.accuracy_with_features(expanded)
+            print("Using feature(s) {", ','.join(map(str, [feature + 1 for feature in expanded])), "} accuracy is",
+                  accuracy, "%")
+            if accuracy > curr_accuracy:
+                curr_accuracy = accuracy
+                set = expanded
+        print("")
+        if curr_accuracy > max_accuracy:
+            max_accuracy = curr_accuracy
+            max_set = set
+        else:
+            print("(Warning, Accuracy has decreased! Continuing search in case of local maxima)")
+        print("Feature set {", ','.join(map(str, [feature + 1 for feature in set])), "} was best, accuracy is",
+              curr_accuracy, "%\n")
+
+    print("Finished search!! The best feature subset is {", ','.join(map(str, [feature + 1 for feature in max_set])),
+          "}, which has an accuracy of", max_accuracy, "%\n")
