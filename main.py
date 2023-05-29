baefdfa89d3c3fdb98f752fedd2e82997155d404
July-Fang2000 <zfang052@ucr.edu>
2023-05-28 22:16:58 -0700

    add: process a real-world classification dataset

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import os
 from preprocess import *
 from method import *
 
@@ -12,12 +14,22 @@ filename = input("Type in the name of the file to test: ")
 #     filename = "CS170_large_Data__32.txt"
 # elif filename == "4":
 #     filename = "CS170_large_Data__33.txt"
+_, file_extension = os.path.splitext(filename)
 
 method = input("Type the number of the algorithm you want to run.\n"
                "     1) Forward Selection\n"
                "     2) Backward Selection\n")
 
-datas, labels = read_txt(filename)
+if file_extension == ".txt":
+    datas, labels = read_txt(filename)
+elif file_extension == ".csv":
+    data = pd.read_csv("./dataset/"+filename)
+    data = data.drop(data.columns[[0, -1]], axis=1)
+    data[data.columns[0]] = data[data.columns[0]].map({'M': 1, 'B': 2})
+    data = data.values
+    labels = data[:, 0]
+    datas = data[:, 1:]
+
 datas = standard_data(datas)
 
 classifier = NearestNeighborClassifier(datas, labels)
